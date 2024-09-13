d6093ce808e4b60a46c0a4646f68ddd455c9fcb1
Martman954 <matej.palas954@gmail.com>
2024-09-13 09:34:52 +0200

    anothe mistakes found

--- Server/Entities/Comment.cs
+++ Server/Entities/Comment.cs
@@ -4,10 +4,14 @@ public class Comment
 {
     public int Id { get; set; }
     public string Body { get; set; }
+    public int PostId { get; private set; }
+    public int UserId { get; private set; }
 
-    public Comment(string body, int id)
+    public Comment(string body, int id, int postId, int userId)
     {
         this.Body = body;
         id = Id;
+        this.PostId = postId;
+        this.UserId = userId;
     }
 }
--- Server/Entities/Post.cs
+++ Server/Entities/Post.cs
@@ -5,11 +5,13 @@ public class Post
     public int Id { get; set; }
     public string Title { get; set; }
     public string Body { get; set; }
+    public int UserId { get; private set; }
 
-    public Post(string title, string body, int id)
+    public Post(string title, string body, int id, int userId)
     {
         this.Title = title;
         this.Body = body;
         this.Id = id;
+        this.UserId = userId;
     }
 }
