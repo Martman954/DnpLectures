87afa25907e5d0e7f4cb42c5dc1ee7828e68c938
Martman954 <matej.palas954@gmail.com>
2024-10-25 00:16:28 +0200

    d

---
+++ Server/WebAPI/Program.cs
@@ -0,0 +1,29 @@
+using FileRepositories;
+using RepositoryContracts;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddSingleton<IUserRepository, UserFileRepository>();
+builder.Services.AddSingleton<IPostRepository, PostFileRepository>();
+builder.Services.AddSingleton<ICommentRepository, CommentFileRepository>();
+
+var app = builder.Build();
+
+app.MapControllers();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment()) {
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+
+app.Run();
